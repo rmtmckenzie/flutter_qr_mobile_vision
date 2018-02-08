8a595b781da1904a9898379d3155bc0e2505baac
Morgan McKenzie <morgan.mckenzie@pipasolutions.com>
2018-02-08 12:13:26 -0800

    Replace ios with objc-only version

--- ios/Classes/QrMobileVisionPlugin.m
+++ ios/Classes/QrMobileVisionPlugin.m
@@ -1,8 +1,20 @@
 #import "QrMobileVisionPlugin.h"
-#import <qr_mobile_vision/qr_mobile_vision-Swift.h>
 
 @implementation QrMobileVisionPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftQrMobileVisionPlugin registerWithRegistrar:registrar];
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"qr_mobile_vision"
+            binaryMessenger:[registrar messenger]];
+  QrMobileVisionPlugin* instance = [[QrMobileVisionPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
 @end
