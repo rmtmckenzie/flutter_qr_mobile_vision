ba825e8b8f554226719422a4c71dc6af29e3e62a
Morgan McKenzie <rmtmckenzie@gmail.com>
2018-07-19 12:54:39 -0700

    Add activity/inactivity detection, add child (overlay), pass through barcode params

--- lib/qr_camera.dart
+++ lib/qr_camera.dart
@@ -7,40 +7,80 @@ import 'package:flutter/rendering.dart';
 import 'package:native_device_orientation/native_device_orientation.dart';
 import 'package:qr_mobile_vision/qr_mobile_vision.dart';
 
+final WidgetBuilder _defaultNotStartedBuilder = (context) => new Text("Camera Loading ...");
+final WidgetBuilder _defaultOffscreenBuilder = (context) => new Text("Camera Paused.");
+final ErrorCallback _defaultOnError = (BuildContext context, Object error) {
+  print("Error reading from camera: $error");
+  return new Text("Error reading from camera...");
+};
+
+typedef Widget ErrorCallback(BuildContext context, Object error);
+
 class QrCamera extends StatefulWidget {
-  QrCamera(
-      {this.fit = BoxFit.cover, this.qrCodeCallback, this.notStartedBuilder})
-      : assert(fit != null);
+  QrCamera({
+    @required this.qrCodeCallback,
+    this.child,
+    this.fit = BoxFit.cover,
+    WidgetBuilder notStartedBuilder,
+    WidgetBuilder offscreenBuilder,
+    ErrorCallback onError,
+    this.formats,
+  })  : notStartedBuilder = notStartedBuilder ?? _defaultNotStartedBuilder,
+        offscreenBuilder = offscreenBuilder ?? notStartedBuilder ?? _defaultOffscreenBuilder,
+        onError = onError ?? _defaultOnError,
+        assert(fit != null);
 
   final BoxFit fit;
   final ValueChanged<String> qrCodeCallback;
+  final Widget child;
   final WidgetBuilder notStartedBuilder;
-
-  void qrCodeHandler(String string) {
-    qrCodeCallback(string);
-  }
+  final WidgetBuilder offscreenBuilder;
+  final ErrorCallback onError;
+  final List<BarcodeFormats> formats;
 
   @override
   QrCameraState createState() => new QrCameraState();
 }
 
-class QrCameraState extends State<QrCamera> {
-  QrCameraState();
+class QrCameraState extends State<QrCamera> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
 
-  PreviewDetails _details;
+  @override
+  dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    print("did change state to $state");
+    if (state == AppLifecycleState.resumed) {
+      setState(() => onScreen = true);
+    } else {
+      if (_asyncInitOnce != null && onScreen) {
+        QrMobileVision.stop();
+      }
+      setState(() {
+        onScreen = false;
+        _asyncInitOnce = null;
+      });
+    }
+  }
+
+  bool onScreen = true;
+  Future<PreviewDetails> _asyncInitOnce;
 
   Future<PreviewDetails> asyncInitOnce(num width, num height) async {
-    if (_details != null) {
-      return _details;
-    }
-
     var previewDetails = await QrMobileVision.start(
       width: width.toInt(),
       height: height.toInt(),
-      qrCodeHandler: widget.qrCodeHandler,
+      qrCodeHandler: widget.qrCodeCallback,
+      formats: widget.formats,
     );
-
-    this._details = previewDetails;
     return previewDetails;
   }
 
@@ -52,51 +92,51 @@ class QrCameraState extends State<QrCamera> {
 
   @override
   Widget build(BuildContext context) {
-    return new LayoutBuilder(
-      builder: (BuildContext context, BoxConstraints constraints) {
-        if (_details == null) {
-          return new SizedBox(
-            width: constraints.maxWidth,
-            height: constraints.maxHeight,
-            child: new FutureBuilder(
-              future:
-                  asyncInitOnce(constraints.maxWidth, constraints.maxHeight),
-              builder: (BuildContext context,
-                  AsyncSnapshot<PreviewDetails> details) {
-                switch (details.connectionState) {
-                  case ConnectionState.none:
-                  case ConnectionState.waiting:
-                    var notStartedBuilder = widget.notStartedBuilder;
-                    return notStartedBuilder == null
-                        ? new Text("Camera Loading ...")
-                        : notStartedBuilder(context);
-                  case ConnectionState.done:
-                    return new Preview(
-                        previewDetails: details.data,
-                        targetWidth: constraints.maxWidth,
-                        targetHeight: constraints.maxHeight,
-                        fit: widget.fit);
-                  default:
-                    throw new AssertionError(
-                        "${details.connectionState} not supported.");
-                }
-              },
-            ),
-          );
-        } else {
-          return SizedBox(
-            width: constraints.maxWidth,
-            height: constraints.maxHeight,
-            child: new Preview(
-              previewDetails: _details,
-              targetWidth: constraints.maxWidth,
-              targetHeight: constraints.maxHeight,
-              fit: widget.fit,
-            ),
-          );
-        }
-      },
-    );
+    return new LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints) {
+      if (_asyncInitOnce == null && onScreen) {
+        _asyncInitOnce = asyncInitOnce(constraints.maxWidth, constraints.maxHeight);
+      } else if (!onScreen) {
+        return widget.offscreenBuilder(context);
+      }
+
+      return new FutureBuilder(
+        future: _asyncInitOnce,
+        builder: (BuildContext context, AsyncSnapshot<PreviewDetails> details) {
+          switch (details.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return widget.notStartedBuilder(context);
+            case ConnectionState.done:
+              if (details.hasError) {
+                return widget.onError(context, details.error);
+              }
+              Widget preview = new SizedBox(
+                width: constraints.maxWidth,
+                height: constraints.maxHeight,
+                child: Preview(
+                  previewDetails: details.data,
+                  targetWidth: constraints.maxWidth,
+                  targetHeight: constraints.maxHeight,
+                  fit: widget.fit,
+                ),
+              );
+
+              if (widget.child != null) {
+                return new Stack(
+                  children: [
+                    preview,
+                    widget.child,
+                  ],
+                );
+              }
+              return preview;
+
+            default:
+              throw new AssertionError("${details.connectionState} not supported.");
+          }
+        },
+      );
+    });
   }
 }
 
@@ -124,8 +164,7 @@ class Preview extends StatelessWidget {
 
     return new NativeDeviceOrientationReader(
       builder: (context) {
-        var nativeOrientation =
-            NativeDeviceOrientationReader.orientation(context);
+        var nativeOrientation = NativeDeviceOrientationReader.orientation(context);
 
         int baseOrientation = 0;
         if (orientation != 0 && (width > height)) {
