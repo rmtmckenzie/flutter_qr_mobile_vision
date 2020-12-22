5b8df134b4ba8df84553abeebbfca0da42441b45
Laxmikanth <laxmikanth.cp@lenskart.in>
2020-12-22 21:07:15 +0530

    Fixed wrong orientation value being returned

--- android/src/main/java/com/github/rmtmckenzie/qrmobilevision/QrCameraC1.java
+++ android/src/main/java/com/github/rmtmckenzie/qrmobilevision/QrCameraC1.java
@@ -58,19 +58,19 @@ class QrCameraC1 implements QrCamera {
         int result;
         switch (rotationCompensation) {
             case 0:
-                result = Surface.ROTATION_0;
+                result = 0;
                 break;
             case 90:
-                result = Surface.ROTATION_90;
+                result = 90;
                 break;
             case 180:
-                result = Surface.ROTATION_180;
+                result = 180;
                 break;
             case 270:
-                result = Surface.ROTATION_270;
+                result = 270;
                 break;
             default:
-                result = Surface.ROTATION_0;
+                result = 0;
                 Log.e(TAG, "Bad rotation value: " + rotationCompensation);
         }
         return result;
